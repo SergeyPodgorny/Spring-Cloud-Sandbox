bff4cc3dd23ba7a5762185e5ee424039c346227d
sergey <sergey@localhost.localdomain>
2022-06-12 07:39:58 +0300

    added new account-service

---
+++ account-service/src/main/java/com/springcloud/account/accountservice/AccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.account.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication.class,args);
+	}
+
+}
