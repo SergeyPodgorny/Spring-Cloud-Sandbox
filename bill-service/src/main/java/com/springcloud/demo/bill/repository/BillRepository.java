71440d93d348c004dbdf21320304431e44683ef2
sergey <sergey@localhost.localdomain>
2022-06-12 09:18:17 +0300

    bill-service has been removed

--- bill-service/src/main/java/com/springcloud/demo/bill/repository/BillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springcloud.demo.bill.repository;
-
-import com.springcloud.demo.bill.entity.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BillRepository  extends JpaRepository<Bill, Long> {
-}
